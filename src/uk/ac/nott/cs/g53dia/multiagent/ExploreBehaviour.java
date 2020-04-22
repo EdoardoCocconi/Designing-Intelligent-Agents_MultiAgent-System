8a010e9f361d59ba8e23e166da2e403b68f5b267
Edoardo <mail>
2020-04-22 23:42:56 +0100

    Fixed out of bounds

--- src/uk/ac/nott/cs/g53dia/multiagent/ExploreBehaviour.java
+++ src/uk/ac/nott/cs/g53dia/multiagent/ExploreBehaviour.java
@@ -2,6 +2,8 @@ package uk.ac.nott.cs.g53dia.multiagent;
 
 import uk.ac.nott.cs.g53dia.multilibrary.*;
 
+import static java.lang.Math.abs;
+
 
 public class ExploreBehaviour extends Behaviour {
 
@@ -13,7 +15,7 @@ public class ExploreBehaviour extends Behaviour {
 
     public Action act(ExploredMap exploredMap) {
 
-        if (agent.previousBehaviour != BehaviourType.EXPLORE_BEHAVIOUR) {
+        if (agent.previousBehaviour != BehaviourType.EXPLORE_BEHAVIOUR || abs(agent.getPosition().getX()) > 199 || abs(agent.getPosition().getY()) > 199) {
             if (agent.getPosition().distanceTo(agent.origin) > 190) {
                 agent.agentDestination = agent.origin;
             } else {
